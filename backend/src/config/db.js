f686e571176a94ae9737beeaf6b3cf9beab07395
BryanUtma <utm24090544@utma.edu.mx>
2026-04-12 19:41:44 -0600

    Cambio

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,14 @@
 const { PrismaClient } = require('@prisma/client');
+const { PrismaPg } = require('@prisma/adapter-pg');
 // PrismaClient → es la clase que genera Prisma para hablar con tu BD
 
 // Crear UNA SOLA instancia de la conexión
 // (como abrir una sola puerta a la base de datos)
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  adapter: new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+  }),
+});
 
 // Exportar para usarla en otros archivos
 module.exports = prisma;
